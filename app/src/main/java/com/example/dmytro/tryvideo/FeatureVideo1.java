d89344bb1991ada14d94b546bd290be0754872e2
Dmytro Ubogiy <dmytroubogiy@gmail.com>
2015-12-03 16:55:16 +0200

    fdgfdg

--- app/src/main/java/com/example/dmytro/tryvideo/FeatureVideo1.java
+++ app/src/main/java/com/example/dmytro/tryvideo/FeatureVideo1.java
@@ -10,6 +10,9 @@ public class FeatureVideo1 {
     //add feature main
     //second change in futremain
     //one more change in featuremain
+<<<<<<< HEAD
     //change in branch1
     //change in branch1 2
+=======
+>>>>>>> parent of dd60463... change in branch1 1
 }
